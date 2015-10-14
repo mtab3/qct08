0bfeeefe48d186fd47f5523d1060abc247d2dc69
M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>
2015-10-14 13:38:02 +0900

    ・プログラムには変更なし ・光がある状態で動作チェックを行った 　15,000点、30回スキャンを行った結果、後半に 3回データ転送ミスがあった。   （ct08 <--> qct08 間の通信で正しいデータ点数よりも少ないデータしか受けられなかった）   しかし、その結果は次の周回のデータには影響を与えなかった。   (読み残しのデータが、次の回の頭に読まれて、スペクトルがシフトするというような現象は起こらなかった)   今後、期待している点数よりも少ない数しかデータを受けられなかった時の対策をもう少し練る。

--- gitlog.h
+++ gitlog.h
@@ -5,6 +5,18 @@
 #define BRANCH_NAME ( "ext" )
 #define GITLOGS ""\
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Tue Oct 13 20:11:09 2015 +0900\n"\
+                "    Merge remote-tracking branch 'origin/ext-chs' into ext-chs\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Tue Oct 13 20:00:24 2015 +0900\n"\
+                "    ext-chs :\n"\"    a) 主に、カウンタで蛍光 quick を行うため、複数チャンネルを束ねた仮想チャンネルを作り\n"\"       束ねられたチャンネルの合計値を取得できるようにした。\n"\"    b) より古い変更だが、動作確認されていない機能として 9999 点を超えるカウントに\n"\"       対応できているはず。\n"\"    \n"\"    光がない状態で下記の点をチェック\n"\"    \n"\"    1) ext-chs の機能(仮想チャンネル)を使わず、かつ データ点数 < 10,000\n"\"    2) ext-chs の機能を使い、かつ データ点数 < 10,000\n"\"    3) ext-chs の機能を使わず、かつ データ点数 >= 10,000\n"\"    4) ext-chs の機能を使い、かつ データ点数 >= 10,000\n"\"    \n"\"    上記、4条件で全て正常。\n"\"    ここで正常としたのは、Function Generator から入れた 5MHz の信号を\n"\"    仮想の測定信号として、30〜50回のクイックスキャンを行い、\n"\"    適切な数値(5,000,000 cps (束ねたチャンネルではこれの整数倍))が\n"\"    適切な点数測定されたことを意味する。\n"\"    従って、おおよそ正常動作しているものと判断できる。\n"\"    \n"\"    但し、\n"\"    上記のチェックでは、全データ点でデータの数値がほぼ同じなので(5,000,000cps)、\n"\"    データがズレても、そのことはわからない。\n"\"    従って、光があるときにこの点をチェック（スペクトルがエネルギーに対してシフトしない）して\n"\"    最終確認とし、stable (or master) にマージする予定\n"\
+		"\n"\
+                "Author      : Masao Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
+                "Date        : Tue Oct 13 19:48:32 2015 +0900\n"\
+                "    checking\n"\
+		"\n"\
+                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Tue Oct 13 18:07:04 2015 +0900\n"\
                 "    modified ct08.cpp to return correct number of datas by QGetData\n"\
 		"\n"\
@@ -31,18 +43,6 @@
                 "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
                 "Date        : Mon Sep 14 17:24:12 2015 +0900\n"\
                 "    merge the change in 'check-dat-num-change'\n"\
-		"\n"\
-                "Author      : Masao Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Tue Aug 11 20:52:55 2015 +0900\n"\
-                "    made sure with no SR\n"\
-		"\n"\
-                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Tue Aug 11 20:32:04 2015 +0900\n"\
-                "    working on extra channels, getValue is ready\n"\
-		"\n"\
-                "Author      : M. Tabuchi <m.tabuchi@nusr.nagoya-u.ac.jp>\n"\
-                "Date        : Tue Aug 11 20:11:20 2015 +0900\n"\
-                "    working on extra chnnels, ReadConf was edited\n"\
 		"\n"\
                 ""
 #endif
